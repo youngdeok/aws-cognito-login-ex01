9e216fdecdc22aef3337816c3456f8023cd089b1
Youngdeok Hwang <medusakiller@gmail.com>
2021-07-03 23:02:55 +1000

    add activities for splash and login

---
+++ app/src/main/java/com/youngdeok/aws_cognito_login_ex01/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.youngdeok.aws_cognito_login_ex01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/youngdeok/aws_cognito_login_ex01/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.youngdeok.aws_cognito_login_ex01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
